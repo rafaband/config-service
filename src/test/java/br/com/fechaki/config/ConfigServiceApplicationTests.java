44268c4c1d2f0df862602ccc15094468d5f3da92
Rafael Bandeira Rodrigues <rafaband@gmail.com>
2023-10-30 10:10:07 -0300

    chore: maven config

---
+++ src/test/java/br/com/fechaki/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fechaki.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
